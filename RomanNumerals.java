3e3a878709eda9460b90bc9f0ef3959e4ade4959
Kamil Ahsan <ahsank8483@gmail.com>
2026-02-16 16:23:07 -0800

    Initial sharing of project

---
+++ RomanNumerals.java
@@ -0,0 +1,37 @@
+
+/**
+ * Program that displays a number from 1-10 as a roman numeral
+ *
+ * @author Kamil Ahsank ahsan@student.sdccd.edu
+ * @since 2/16/2026
+ * @version v1.0
+ *
+ */
+
+import java.util.Scanner;
+
+public class RomanNumerals {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number between 1 and 3: ");
+        int number = input.nextInt();
+        
+        switch (number) {
+            case 1:
+                System.out.println("The Roman numeral version is: I");
+                break;
+            case 2:
+                System.out.println("The Roman numeral version is: II");
+                break;
+            case 3:
+                System.out.println("The Roman numeral version is: III");
+                break;
+            default:
+                System.out.println("Error: Not between 1 and 3");
+                break;
+        }
+        
+        input.close();
+    }
+}
